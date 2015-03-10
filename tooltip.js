8f55f3bbd09d5e0d721a9ca3cb9e2cb7227ec5f1
Natarajan Shanker <nshanker@paypal.com>
2015-03-10 16:12:37 -0700

    Some cleanup

--- tooltip.js
+++ tooltip.js
@@ -1,6 +1,8 @@
 var Utils = Utils || {};
 
-Utils.Tooltip = function(el) {
+Utils.Tooltip = function (el) {
+	'use strict';
+
 	this.el = el;
 
 	this.buildDOM();
@@ -8,40 +10,43 @@ Utils.Tooltip = function(el) {
 };
 
 Utils.Tooltip.prototype.attachEvents = function() {
+	'use strict';
+
 	var el = this.el,
 		tooltip = this.tooltip,
-		tipText = tooltip.getElementsByTagName("p")[0],
-		tipArrow = tooltip.getElementsByTagName("span")[0];
+		tipText = tooltip.getElementsByTagName('p')[0];
 
-	el.addEventListener("focus", showTooltip);
-	el.addEventListener("blur", hideTooltip);
-
-	function showTooltip() {
-		tooltip.style.left = el.offsetLeft+ "px";
-		tooltip.style.top = el.offsetTop + el.offsetHeight + 7 + "px";
-		tooltip.style.display = "block";
+	function showTooltip () {
+		tooltip.style.left = el.offsetLeft+ 'px';
+		tooltip.style.top = el.offsetTop + el.offsetHeight + 7 + 'px';
+		tooltip.style.display = 'block';
 
 		tipText.innerHTML = el.title;
 	}
 
-	function hideTooltip() {
-		tooltip.style.display = "none";
+	function hideTooltip () {
+		tooltip.style.display = 'none';
 	}
-}
 
-Utils.Tooltip.prototype.buildDOM = function() {
-	var theTooltip = document.getElementById("tooltip"),
-		body = document.getElementsByTagName("body")[0],
+	el.addEventListener('focus', showTooltip);
+	el.addEventListener('blur', hideTooltip);
+};
+
+Utils.Tooltip.prototype.buildDOM = function () {
+	'use strict';
+
+	var theTooltip = document.getElementById('tooltip'),
+		body = document.getElementsByTagName('body')[0],
 		tipText,
 		tipArrow;
 
-	if(!theTooltip){
-		theTooltip = document.createElement("div");
-		theTooltip.id = "tooltip";
-		theTooltip.className = "tooltip";
+	if (!theTooltip) {
+		theTooltip = document.createElement('div');
+		theTooltip.id = 'tooltip';
+		theTooltip.className = 'tooltip';
 
-		tipArrow = document.createElement("span");
-		tipText = document.createElement("p");
+		tipArrow = document.createElement('span');
+		tipText = document.createElement('p');
 
 		body.appendChild(theTooltip);
 		theTooltip.appendChild(tipText);
@@ -51,15 +56,14 @@ Utils.Tooltip.prototype.buildDOM = function() {
 	this.tooltip = theTooltip;
 
 	return;
-}
+};
 
-window.addEventListener("load", function() {
-	"use strict";
+window.addEventListener('load', function () {
+	'use strict';
 
-	var inputs = document.getElementsByClassName("showTooltip");
+	var inputs = document.getElementsByClassName('showTooltip');
 
-	Array.prototype.forEach.call(inputs, function(key, index, array) {
+	Array.prototype.forEach.call(inputs, function (key) {
 		new Utils.Tooltip(key);
-	});	
+	});
 });
-
